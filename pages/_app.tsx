5663d2f9f26d2d9aa845d7b43df87566ae50e22f
Raicuparta <raicuparta@gmail.com>
2021-07-16 16:02:29 +0200

    Material

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme, CssBaseline, ThemeProvider } from '@material-ui/core';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#e4fffe',
+      paper: '#ffffff',
+    },
+    text: {
+      secondary: '#276777',
+    }
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-export default MyApp
+
+export default MyApp;
