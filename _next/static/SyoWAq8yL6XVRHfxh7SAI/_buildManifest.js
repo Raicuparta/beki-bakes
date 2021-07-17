6995f25db634bdfcf263ba18ffc0c1a3ec5f9be0
Raicuparta <raicuparta@gmail.com>
2021-07-17 12:26:42 +0200

    Updates

--- _next/static/SyoWAq8yL6XVRHfxh7SAI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/69-f14c60c9cbf0d684ee41.js","static/chunks/pages/index-35ee6def23122b5d2a3f.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
