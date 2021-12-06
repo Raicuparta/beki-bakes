036214052c9773d7e9886622c4aae355cf75ae34
Raicuparta <raicuparta@gmail.com>
2021-12-06 12:35:43 +0100

    Rever scroll restoration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
-import React, { useEffect } from "react";
+import React from "react";
 import { Grid } from "@mui/material";
-import { useRouter } from "next/dist/client/router";
 
 import { products } from "../components";
 import { ListedProduct } from "../components/ListedProduct";
@@ -9,41 +8,28 @@ import { PageContainer } from "../components/PageContainer";
 import { productPhotos } from "../components/productPhotos";
 import { BreadcrumbsList } from "../components/BreadcrumbsList";
 
-const Home = () => {
-  const { beforePopState } = useRouter();
-
-  useEffect(() => {
-    beforePopState((state) => {
-      // Disable scroll restoration on navigation,
-      // to prevent messy animations.
-      state.options.scroll = false;
-      return true;
-    });
-  }, [beforePopState]);
-
-  return (
-    <>
-      <Head>
-        <title>bekibakes</title>
-      </Head>
-      <main>
-        <PageContainer>
-          <BreadcrumbsList items={[{ title: "Products" }]} />
-          <Grid container spacing={2} sx={{ mb: 2 }}>
-            {Object.values(products).map(({ id, name }) => (
-              <ListedProduct
-                key={id}
-                href={`/${id}`}
-                name={name}
-                image={productPhotos[id]["main"]}
-                price={0}
-              />
-            ))}
-          </Grid>
-        </PageContainer>
-      </main>
-    </>
-  );
-};
+const Home = () => (
+  <>
+    <Head>
+      <title>bekibakes</title>
+    </Head>
+    <main>
+      <PageContainer>
+        <BreadcrumbsList items={[{ title: "Products" }]} />
+        <Grid container spacing={2} sx={{ mb: 2 }}>
+          {Object.values(products).map(({ id, name }) => (
+            <ListedProduct
+              key={id}
+              href={`/${id}`}
+              name={name}
+              image={productPhotos[id]["main"]}
+              price={0}
+            />
+          ))}
+        </Grid>
+      </PageContainer>
+    </main>
+  </>
+);
 
 export default Home;
