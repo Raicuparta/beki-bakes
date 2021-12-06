630848b1d6e5f41d16ec7885cdd131b597e70023
Raicuparta <raicuparta@gmail.com>
2021-12-06 20:14:39 +0100

    ok what else

--- pages/menu.tsx
+++ pages/menu.tsx
@@ -10,7 +10,23 @@ import { PageContainer } from "../components/PageContainer";
 import { productPhotos } from "../components/productPhotos";
 import { BreadcrumbsList } from "../components/BreadcrumbsList";
 
-const size = 60;
+const snailSize = 80;
+
+type Props = {
+  children: React.ReactNode;
+  title: string;
+  price: number;
+};
+
+const MenuItem = (props: Props) => (
+  <Stack sx={{ alignItems: "center" }}>
+    {props.children}
+    <Stack sx={{ alignItems: "center" }}>
+      <Typography variant="h5">{props.title}</Typography>
+      <Typography fontSize={20}>€{props.price}</Typography>
+    </Stack>
+  </Stack>
+);
 
 const Menu = () => (
   <>
@@ -18,67 +34,83 @@ const Menu = () => (
       <title>bekibakes</title>
     </Head>
     <main>
-      <Box
+      <Stack
         sx={{
           width: 540,
           height: 1920 / 2,
-          background: "white",
           margin: "auto",
-          p: 2,
-          color: "text.secondary",
         }}
       >
         <Stack
-          direction={"column"}
+          direction="row"
+          spacing={1}
           sx={{
-            pb: 1,
-            alignItems: "center",
+            p: 2,
+            alignItems: "flex-end",
             justifyContent: "center",
+            position: "relative",
+            bgcolor: "background.paper",
+          }}
+        >
+          <Typography
+            sx={{
+              fontWeight: "bold",
+              fontSize: 30,
+              lineHeight: 1,
+            }}
+          >
+            menu
+          </Typography>
+        </Stack>
+        <Box
+          sx={{
+            background: "white",
+            p: 2,
+            color: "text.secondary",
+            flex: 1,
             position: "relative",
           }}
         >
-          <Image
-            src="/snail-halfwhite.svg"
-            alt="beki bakes"
-            width={size}
-            height={size}
-            priority
-          />
-          <Typography
-            component="h1"
-            align="center"
+          <Box
             sx={{
-              fontWeight: "bold",
-              fontSize: 25,
+              position: "absolute",
+              right: (theme) => theme.spacing(2),
+              bottom: (theme) => theme.spacing(2),
             }}
           >
-            bekibakes
-          </Typography>
-        </Stack>
-        <Stack>
-          <Stack direction="row" spacing={2} sx={{ alignItems: "center" }}>
-            <RollCakeIcon
-              outlineColor="#677d2b"
-              cakeColor="#a3b554"
-              fillingColor="#a44b3c"
+            <Image
+              src="/snail-halfwhite.svg"
+              alt="beki bakes"
+              width={snailSize}
+              height={snailSize}
+              priority
             />
-            <Stack>
-              <Typography>Matcha outside azuki inside hehe</Typography>
-              <Typography>Azuki inside hehe</Typography>
-            </Stack>
+          </Box>
+          <Stack spacing={2}>
+            <MenuItem title="Matcha Azuki Roll" price={20}>
+              <RollCakeIcon
+                outlineColor="#677d2b"
+                cakeColor="#a5ad55"
+                fillingColor="#a44b3c"
+              />
+            </MenuItem>
+            <MenuItem title="Chocolate Chestnut Roll" price={20}>
+              <RollCakeIcon
+                outlineColor="#251d17"
+                cakeColor="#4c392b"
+                fillingColor="#a58a71"
+              />
+            </MenuItem>
+            <MenuItem title="Vanilla Salted Caramel Roll" price={20}>
+              <RollCakeIcon
+                outlineColor="#b8954a"
+                cakeColor="#ebd691"
+                fillingColor="#d09e6f"
+              />
+            </MenuItem>
           </Stack>
-          <RollCakeIcon
-            outlineColor="#251d17"
-            cakeColor="#4c392b"
-            fillingColor="#a58a71"
-          />
-          <RollCakeIcon
-            outlineColor="#b8954a"
-            cakeColor="#ebd691"
-            fillingColor="#d09e6f"
-          />
-        </Stack>
-      </Box>
+        </Box>
+      </Stack>
     </main>
   </>
 );
