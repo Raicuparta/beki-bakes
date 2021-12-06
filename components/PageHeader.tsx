44e4506f2d321ea1cb52d282f5e811eb4ea9f60c
Raicuparta <raicuparta@gmail.com>
2021-12-06 12:27:07 +0100

    Fix header

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -4,57 +4,50 @@ import Image from "next/image";
 import { SocialIcons } from ".";
 import Link from "next/link";
 
-const size = 75;
+const size = 60;
 
-export const PageHeader = () => {
-  return (
-    <Box
-      component="a"
-      sx={{
-        all: "unset",
-        backgroundColor: "background.paper",
-        display: "block",
-        mb: 2,
-        width: "100%",
-        top: 0,
-        zIndex: 1,
-      }}
-    >
-      <Container maxWidth="sm" sx={{ position: "relative" }}>
-        <Link passHref href="/">
-          <Stack
-            component={Button}
-            direction={"column"}
+export const PageHeader = () => (
+  <Box
+    sx={{
+      bgcolor: "background.paper",
+      mb: 2,
+      width: "100%",
+    }}
+  >
+    <Container maxWidth="sm" sx={{ position: "relative" }}>
+      <Link passHref href="/">
+        <Stack
+          component={Button}
+          direction={"column"}
+          sx={{
+            pt: 2,
+            pb: 1,
+            color: "text.primary",
+            alignItems: "center",
+            justifyContent: "center",
+            position: "relative",
+          }}
+        >
+          <Image
+            src="/snail-halfwhite.svg"
+            alt="beki bakes"
+            width={size}
+            height={size}
+            priority
+          />
+          <Typography
+            component="h1"
+            align="center"
             sx={{
-              pt: 2,
-              pb: 1,
-              color: "text.primary",
-              alignItems: "center",
-              justifyContent: "center",
-              position: "relative",
+              fontWeight: "bold",
+              fontSize: 25,
             }}
           >
-            <Image
-              src="/snail-halfwhite.svg"
-              alt="beki bakes"
-              width={size}
-              height={size}
-              priority
-            />
-            <Typography
-              component="h1"
-              align="center"
-              sx={{
-                fontWeight: "bold",
-                fontSize: 30,
-              }}
-            >
-              bekibakes
-            </Typography>
-          </Stack>
-        </Link>
-        <SocialIcons />
-      </Container>
-    </Box>
-  );
-};
+            bekibakes
+          </Typography>
+        </Stack>
+      </Link>
+      <SocialIcons />
+    </Container>
+  </Box>
+);
