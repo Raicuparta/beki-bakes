e1837f103761aa31dc97df72d03f72f2c7893a53
Raicuparta <raicuparta@gmail.com>
2022-02-19 12:18:02 +0100

    Padding

--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -2,5 +2,7 @@ import { Container } from "@mui/material";
 import React from "react";
 
 export const PageContainer: React.FC = ({ children }) => (
-  <Container maxWidth="sm">{children}</Container>
+  <Container maxWidth="sm" sx={{ pb: 2 }}>
+    {children}
+  </Container>
 );
